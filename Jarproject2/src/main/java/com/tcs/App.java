198a135272a96318296a38ce5b846385b84e7264
Poojapatilll <poojapatil814745@gmail.com>
2024-11-06 04:54:01 -0800

    ReverseString

---
+++ Jarproject2/src/main/java/com/tcs/App.java
@@ -0,0 +1,15 @@
+package com.tcs;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        String str="java";
+        String result="";
+        for(int i=str.length()-1;i>=0;i--){
+            result=result+str.charAt(i);
+        }
+        System.out.println(result);
+    }
+}
